b7bdbd77e7411a6340f18c96a5d781bf8186b81f
Valentin Popov <info@valentineus.link>
2020-02-20 11:20:00 +0400

    Service's method structure
    Signed-off-by: Valentin Popov <info@valentineus.link>

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,36 @@
 export declare class Service {
 	public constructor(busId: string, activityManager?: IActivityManager, options?: IServiceOptions);
+
+	private _dispatch(): any;
+
+	private _register(): any;
+
+	private _registerBuiltInMethods(): any;
+
+	public call(): any;
+
+	public cancelSubscription(): any;
+
+	public cleanupUnified(): any;
+
+	public idIsPrivileged(): any;
+
+	public info(message: IMessage): void;
+
+	public quit(message: IMessage): void;
+
+	public register(): any;
+
+	public registerPrivate(): any;
+
+	public subscribe(uri: string, args: any): Subscription;
 }
 
+export declare class Subscription { }
+
 export interface IActivityManager { }
+
+export interface IMessage { }
 
 export interface IServiceOptions {
 	readonly idleTimer?: number;
